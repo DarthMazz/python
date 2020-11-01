7f0f42cd5b3ca5ae2e0f0766741e57948914f2b7
松本義高 <yma2@matsumotoyoshitakanoMacBook-Pro.local>
2020-11-01 17:15:15 +0900

    add argparse

--- args/argv.py
+++ args/argv.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 
 def analyze_argv(argv):
@@ -20,7 +21,48 @@ def analyze_argv(argv):
         print("some argv.")
 
 
+def get_args():
+    """
+    引数取得
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    parser = argparse.ArgumentParser()
+
+    if sys.stdin.isatty():
+        parser.add_argument("file", help="please set me", type=str)
+
+    parser.add_argument("--type", type=int)
+    parser.add_argument("--alert", help="optional", action="store_true")
+
+    args = parser.parse_args()
+
+    return (args)
+
+
+def main():
+    """
+    メイン処理
+    """
+    args = get_args()
+
+    if hasattr(args, 'file'):
+        print(args.file)
+    print(args.type)
+    print(args.alert)
+
+    if args.alert:
+        None
+
+
 if __name__ == "__main__":
     argv = sys.argv
     print(argv)
     analyze_argv(argv)
+    main()
