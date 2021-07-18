a4a81e50fffe4f6ec648daf536535b1226683482
yo4taka.ma2moto <yo4taka.ma2moto@gmail.com>
2021-07-18 12:09:08 +0900

    add comment backoff_factor

--- http/http_request.py
+++ http/http_request.py
@@ -3,7 +3,7 @@ from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 
 
-class UrlRequest:
+class HttpRequest:
     def __init__(self, url):
         self.url = url
 
@@ -12,20 +12,27 @@ class UrlRequest:
 
     def request(self):
         s = requests.Session()
-        retries = Retry(total=2,
-                        backoff_factor=1,
+        # backoff_factor = 3
+        # for retry_count in range(1, 6):
+        #     print(f'backoff_factor : {backoff_factor}, retry_count : {retry_count}, retry_time: {backoff_factor * (2 ** (retry_count - 1))}')
+        retries = Retry(total=3, #リトライ回数
+                        backoff_factor=1, #リトライ間隔(backoff_factor * (2 ** (retry_count - 1))), https://urllib3.readthedocs.io/en/latest/reference/urllib3.util.html
                         status_forcelist=[500, 502, 503, 504])
         s.mount('https://', HTTPAdapter(max_retries=retries))
         s.mount('http://', HTTPAdapter(max_retries=retries))
+        # connect timeoutを10秒, read timeoutを30秒に設定
         r = s.request('GET', self.url, timeout=(10.0, 30.0))
+        # ステータスコードが200番台以外だったら例外を起こす
         r.raise_for_status()
+        return r
 
 
 def main():
     url = 'http://localhost:5000'
-    url_request = UrlRequest(url)
+    http_request = HttpRequest(url)
     try:
-        url_request.request()
+        response = http_request.request()
+        # print(response.text)
     except requests.RequestException:
         print(f'request error {url}')
 
