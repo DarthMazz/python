84b9588353d386a4e15e05b46e59118e5eb86afb
YOSHITAKA Matsumoto <yo4taka.ma2moto@gmail.com>
2021-11-22 22:13:15 +0900

    add file download

--- pyppeteer/application.py
+++ pyppeteer/application.py
@@ -1,5 +1,9 @@
 import asyncio
 from pyppeteer import launch
+import time
+import random
+import urllib
+import os
 
 
 async def html_parse(html):
@@ -26,12 +30,14 @@ async def main():
     print("ウェブサイトを開く")
     await page.goto("https://www.google.co.jp/advanced_image_search")
 
+    time.sleep(5+random.randrange(10))
     await page.type('#xX4UFf', '請求書')
 
     # スクリーンショット取得
     print("スクリーンショット1を保存")
     await page.screenshot({"path": "./sceeenshot1.png"})
 
+    time.sleep(5+random.randrange(10))
     await asyncio.wait([
         page.click('input[value="詳細検索"]'),
         page.waitForNavigation(),
@@ -39,6 +45,26 @@ async def main():
 
     print("スクリーンショット2を保存")
     await page.screenshot({"path": "./sceeenshot2.png"})
+
+    time.sleep(5+random.randrange(10))
+    a_images = await page.JJ('a[class="wXeWr islib nfEiy"]')
+    await asyncio.wait([
+        a_images[0].click(),
+        page.waitForNavigation(),
+    ])
+
+    print("スクリーンショット3を保存")
+    await page.screenshot({"path": "./sceeenshot3.png"})
+    time.sleep(5+random.randrange(10))
+    image = await page.JJ('img[class="n3VNCb"]')
+    src = await image[0].getProperty('src')
+    targetUrl = await src.jsonValue()
+    download_file_name = os.path.basename(urllib.parse.urlparse(targetUrl).path)
+    viewSource = await page.goto(targetUrl)
+    time.sleep(5+random.randrange(10))
+    image_buffer = await viewSource.buffer()
+    with open(f'./{download_file_name}', 'wb') as f:
+        f.write(image_buffer)
 
     # ブラウザ停止
     await browser.close()
